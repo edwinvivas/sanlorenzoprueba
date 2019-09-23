c08e250b6f631a2d2f5f7bc4fcf5ba7c4e34c8a0
EdwinVivas <edwinvivassoto@gmail.com>
2019-09-23 15:13:02 -0500

    Se agrego , modelo de formulario

--- SB-Admin-BS4-Angular-8/src/app/layout/blank-page2/blank-page2.module.ts
+++ SB-Admin-BS4-Angular-8/src/app/layout/blank-page2/blank-page2.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PageHeaderModule } from './../../shared';
 
 import { BlankPage2RoutingModule } from './blank-page2-routing.module';
 import { BlankPage2Component } from './blank-page2.component';
 
 @NgModule({
-    imports: [CommonModule, BlankPage2RoutingModule],
+    imports: [CommonModule, BlankPage2RoutingModule ,PageHeaderModule],
     declarations: [BlankPage2Component]
 })
 export class BlankPage2Module {}
